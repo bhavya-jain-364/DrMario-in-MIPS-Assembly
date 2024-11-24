226aaa62496ec285ed0c2e2ec7140eda518a7ee8
Bhavya Jain <76478989+bhavya-jain-364@users.noreply.github.com>
2024-11-23 21:04:42 -0500

    implemented sound effects and game over

--- png_to_mips.py
+++ png_to_mips.py
@@ -35,11 +35,11 @@ def convert_png_to_mapped_pixels(image_path, output_width, output_height, x_offs
     return results
 
 # Example usage
-image_path = "capsules.png"  # Replace with the path to your PNG
-output_width =  30 # Desired width
-output_height = 50  # Desired height
-x_offset = 37  # Starting X position in bitmap
-y_offset = 76  # Starting Y position in bitmap
+image_path = "game over.png"  # Replace with the path to your PNG
+output_width =  64 # Desired width
+output_height = 32  # Desired height
+x_offset = 0  # Starting X position in bitmap
+y_offset = 16  # Starting Y position in bitmap
 
 pixel_data = convert_png_to_mapped_pixels(image_path, output_width, output_height, x_offset, y_offset)
 
